d3ce701b878db82b285766beb7111cd2dc403df6
Jesse Hallett <jesse@sitr.us>
2018-06-07 13:59:29 -0400

    Remove service worker (LP #45257971)

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./frontend/App"
-import registerServiceWorker from "./frontend/registerServiceWorker"
 
 const elem = document.getElementById("root")
 if (elem) {
 	ReactDOM.render(<App />, elem)
-	registerServiceWorker()
 } else {
 	throw new Error("Unable to locate mount point for React app in DOM")
 }
